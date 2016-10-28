f203a6157c5c7d9288fd86471a680c78f0abb017
yujian <zhaosukima@gmail.com>
2016-10-27 20:23:56 -0700

    8000khz rate

--- client/subrecord.py
+++ client/subrecord.py
@@ -4,18 +4,19 @@ import numpy
 import time
 import os
 import sys
+import math
 
 record_proc = subprocess.Popen(["arecord","-fS16_LE","-r8000","-c1","-traw"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-rate=mass.floor(8000/100)
+rate=math.floor(8000/100)
 
-amp=numpy.zeros((rate,))
-th=numpy.zeros((rate,))
+amp=numpy.zeros((80,))          #rate
+th=numpy.zeros((80,))           #rate
 
 f=open('testpy.raw','wb')
 f.write(record_proc.stdout.read(1))
 while 1:
-    for i in range(0,rate-1):
+    for i in range(0,79):                   #rate
         data=record_proc.stdout.read(2)
         a= ord(data[0])*256+ord(data[1])
         if a>32767:
@@ -26,7 +27,7 @@ while 1:
 ##noise level evaluation endis
     noise = amp.mean()
     while 1:
-        for i in range(0,rate-1):
+        for i in range(0,79):               #rate
             data=record_proc.stdout.read(2)
             #record in buffer
             a= ord(data[0])*256+ord(data[1])
@@ -38,11 +39,11 @@ while 1:
         if amp.mean()>2.5*noise:
             print('start:noise:',noise,'voice:',amp.mean())
             #write buffer into file
-            th=numpy.zeros((rate))
+            th=numpy.zeros((80))          #rate
             silent=0
 ##start capture voice
             while silent<300:    
-                for i in range(0,rate-1):
+                for i in range(0,79):       #rate
                     data=record_proc.stdout.read(2)
                     f.write(data)
                     a= ord(data[0])*256+ord(data[1])
